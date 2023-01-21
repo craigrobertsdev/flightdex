f356f8c61bcbd0caaaf3f6c743dc408a1695f37f
Craig Roberts <craig.roberts11@outlook.com>
2023-01-22 06:33:12 +1030

    add url builder and event type filter

--- assets/js/event-results.js
+++ assets/js/event-results.js
@@ -12,28 +12,49 @@ import Geohash from 'https://cdn.jsdelivr.net/npm/latlon-geohash@2.0.0';
 */
 
 const resultsSection = $('#results');
-const queryString = '?lat=41.881832&long=-87.623177&arrival=2023-02-25&departure=2023-03-15'.split(
-  '&'
-);
+const filterOptions = $('#filter-options');
+const radiusInput = $('#radius-input');
+const keywordInput = $('#keyword-input');
+const filterButton = $('#filter-button');
+const searchButton = $('#event-search-button');
+
+// test query string
+const queryString = '?lat=41.881832&long=-87.623177&arrival=2023-02-25&departure=2023-03-15'.split('&'); //location.search
+
+// stores different options for building a query string
+const options = {};
 
 const latitude = queryString[0].split('=')[1];
 const longitude = queryString[1].split('=')[1];
-const arrivalDate = queryString[2].split('=')[1];
-const departureDate = queryString[3].split('=')[1];
 
-const geoHash = Geohash.encode(latitude, longitude, 6);
+options.startDateTime = queryString[2].split('=')[1];
+options.endDateTime = queryString[3].split('=')[1];
+options.geoPoint = Geohash.encode(latitude, longitude, 6);
+// Default radius to search for from accommodation location. Can be modified by user in radiusInput
+options.radius = 30;
 
-// Radius from accommodation location. Can be set by the user as part of form submission
-let radius = 30;
+let url = buildUrl(options);
 
 // data from API query
 let resultData;
 
-const requestUrl = `https://app.ticketmaster.com/discovery/v2/events.json?geoPoint=${geoHash}&radius=${radius}&startDateTime=${arrivalDate}T00:00:00Z&endDateTime=${departureDate}T00:00:00Z&apikey=Rokm7oUpGBonFqFDXXiA7tcSkqAaiQh4`;
-function getEvents() {
-  // event.preventDefault();
+// takes an options object, iterates over it and produces a query string that the TicketMaster API accepts.
+function buildUrl(options) {
+  let urlArr = ['https://app.ticketmaster.com/discovery/v2/events.json?apikey=Rokm7oUpGBonFqFDXXiA7tcSkqAaiQh4&size=200'];
 
-  fetch(requestUrl)
+  for (let key in options) {
+    urlArr.push('&', key, '=', options[key]);
+    if (key === 'endDateTime' || key === 'startDateTime') {
+      urlArr.push('T00:00:00Z');
+    }
+  }
+
+  return urlArr.join('');
+}
+
+function getEvents(url) {
+  // TODO - replace hard coded string with generated url for final version
+  fetch(url)
     .then((response) => {
       // if fetch request successful, return response in JSON format
       if (response.ok) {
@@ -42,33 +63,26 @@ function getEvents() {
     })
     // save response data for future use
     .then((data) => {
-      resultData = data;
+      resultData = data._embedded.events;
       console.log(resultData);
-      displayResults();
+      determineEventTypes();
+      displayResults(resultData);
     })
     .catch((reason) => {
       console.log('There was an error in processing the request: ' + reason);
     });
-
-  console.log(resultData);
 }
 
-function displayResults() {
+// clears the current resultData section and displays the currently filtered options
+function displayResults(resultData) {
+  $(resultsSection).html('');
   for (let i = 0; i < 10; i++) {
-    // const eventName = resultData._embedded.events[i].name;
-    // const startDate = resultData._embedded.events[i].dates.start.localDate;
-    // const startTime = resultData._embedded.events[i].dates.start.localTime;
-    // const priceRangeMin = resultData._embedded.events[i].priceRanges[0].min;
-    // const genre = resultData._embedded.events[i].classifications[0].genre.name;
-    // const eventUrl = resultData._embedded.events[i].url;
-
-    // Test data to develop with without repeated API calls
-    const eventName = 'Hockey game';
-    const startDate = '15/03/2023';
-    const startTime = '12:00pm';
-    const priceRangeMin = '$50';
-    const genre = 'Hockey';
-    const eventUrl = 'http://ticketmaster.com';
+    const eventName = resultData[i].name;
+    const startDate = resultData[i].dates.start.localDate;
+    const startTime = resultData[i].dates.start.localTime;
+    const priceRangeMin = resultData[i].priceRanges[0].min;
+    const genre = resultData[i].classifications[0].genre.name;
+    const eventUrl = resultData[i].url;
 
     const eventHeaderEl = $('<p></p>').text(eventName).addClass('header');
     const dateTimeEl = $('<p></p>').addClass('date-time');
@@ -81,14 +95,60 @@ function displayResults() {
       .addClass('price-range');
 
     const genreEl = $('<p></p>').text(genre).addClass('genre');
-    const eventUrlEl = $('<a></a>')
-      .attr('href', eventUrl)
-      .attr('target', '_blank')
-      .text('Link to event booking')
-      .addClass('link');
+    const eventUrlEl = $('<a></a>').attr('href', eventUrl).attr('target', '_blank').text('Link to event booking').addClass('link');
 
-    $('#results').append(eventHeaderEl, dateTimeEl, priceRangeEl, genreEl, eventUrlEl);
+    $(resultsSection).append(eventHeaderEl, dateTimeEl, priceRangeEl, genreEl, eventUrlEl);
   }
 }
 
-getEvents();
+// Adds functionality to the filter button to filter displayed results by the user selected event type
+$(filterButton).on('click', function filterByEventType(event) {
+  event.preventDefault();
+  const eventType = $(filterOptions).val();
+
+  const filteredOptions = resultData.filter((event) => {
+    return event.classifications[0].segment.name === eventType;
+  });
+
+  displayResults(filteredOptions);
+});
+
+// iterates over result set and determines unique event types before adding them to the filter options dropdown list.
+function determineEventTypes() {
+  const eventTypes = [];
+  for (let i = 0; i < resultData.length; i++) {
+    let eventType = resultData[i].classifications[0].segment.name;
+    if (!eventTypes.includes(eventType)) {
+      eventTypes.push(eventType);
+    }
+  }
+
+  for (let type of eventTypes) {
+    const option = $('<option></option>').val(type).text(type);
+    $(filterOptions).append(option);
+  }
+}
+
+// when user click search button, a new query string is created with currently entered values and data is retrieved from the API
+$(searchButton).on('click', function (event) {
+  event.preventDefault();
+  const queryOpts = {};
+  if ($(radiusInput).val()) {
+    // API uses miles but site is built for users who deal in km
+    radius = convertToMiles($(radiusInput).val());
+    queryOpts.radius = radius;
+  }
+
+  if ($(keywordInput.val())) {
+    queryOpts.keyword = $(keywordInput.val());
+  }
+
+  const queryString = buildUrl(queryOpts);
+  getEvents(queryString);
+});
+
+function convertToMiles(km) {
+  return km * 0.621371;
+}
+
+getEvents(url);
