f360e11f3a605f0316896bbc9a043872d30d43d9
hyunsik kim (kayden) <kayden190116@gamil.com>
2023-01-22 05:01:41 +1030

    Token will be newly generated every 20mins and fetched data finding IATA CODE WITH city keyword API

--- assets/js/search-results.js
+++ assets/js/search-results.js
@@ -1,4 +1,11 @@
+
+
+getToken() // this will be coroperated with input data submit button
+
+
+
 // generate new access-token whenever app starts
+function getToken(){
 const TOKENUrl = "https://api.amadeus.com/v1/security/oauth2/token"
 
 fetch(TOKENUrl, {
@@ -15,6 +22,26 @@ fetch(TOKENUrl, {
     console.log(data.access_token);
     getDATA(data);
   });
+}
+
+// fetching iATA code data with city keyword with the new generated access-token
+function getIATAcodeDATA(data) {
+  let accessToken = data.token_type +" " +data.access_token;
+  console.log(accessToken);
+
+  let requestUrl = "https://api.amadeus.com/v1/reference-data/locations?subType=CITY&keyword=ADEN&page%5Blimit%5D=10&page%5Boffset%5D=0&sort=analytics.travelers.score&view=FULL"
+
+  fetch(requestUrl, {
+    headers: {Authorization: accessToken}
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log('data Response \n-------------');
+      console.log(data);
+    });
+}
 
 // fetching data with the new generated access-token
 function getDATA(data) {
@@ -33,3 +60,8 @@ function getDATA(data) {
       console.log(data);
     });
 }
+
+timeInterval = setInterval(() => {
+  getToken();
+}, 900000) // the token will be generated every 20mins - if you want to test it, change the number to 10000 then will be generated every 10 seconds
+
