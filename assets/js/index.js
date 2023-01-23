8e86bc0e101e727a6ff1b47c4225e78ba541c28a
mushroom <shaelah.mai@gmail.com>
2023-01-23 20:21:33 +1000

    added bulma date picker and debugged the index.html

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,22 @@
-$( function() {
-    $( "#datepicker" ).datepicker({
-      changeMonth: true,
-      changeYear: true
-    });
-  } );
+// Initialize all input of type date
+var calendars = bulmaCalendar.attach('[type="date"]', {
+  isRange: true
+});
+
+// Loop on each calendar initialized
+for(var i = 0; i < calendars.length; i++) {
+	// Add listener to select event
+	calendars[i].on('select', date => {
+		console.log(date.data.value());
+    console.log(date.data.startDate)
+	});
+}
+
+// To access to bulmaCalendar instance of an element
+var element = document.querySelector('#my-element');
+if (element) {
+	// bulmaCalendar instance is available as element.bulmaCalendar
+	element.bulmaCalendar.on('select', function(datepicker) {
+		console.log(datepicker.data.value());
+	});
+}
