7faafb757266b478eec7b0c8315e28d64294ed78
LucienPep <xxyyzzoozz.com>
2023-01-24 18:08:03 +1030

    Added token generator and API to get data for hotels at destination / Added called elements into HTML

--- assets/js/hotel-results.js
+++ assets/js/hotel-results.js
@@ -1,4 +1,6 @@
-var amadeus = ''
+var results = document.getElementById('results')
+var tokenType = ''
+
 
 async function tokenFunction() {
     let response = await fetch('https://test.api.amadeus.com/v1/security/oauth2/token',{
@@ -8,24 +10,25 @@ async function tokenFunction() {
         method: 'POST',
         body: "grant_type=client_credentials&client_id=BrtG02hEbIco7uiyR3DRJAvGMMtMHVbe&client_secret=piDWDBPQ5cjMQOdm"     
     })
-    
         console.log(response.status)
+        response = await response.json()
         console.log(response)
-    
+        var token = response.access_token
+        console.log(token)
+        tokenType = ('Bearer ' + token)
+        console.log(tokenType)
         cityCoordinates('ADL')
     
 }
-
-//https://test.api.amadeus.com/v1/reference-data/locations/hotels/by-city?cityCode=PAR
-
 
 async function cityCoordinates(city){
     console.log(city)
     var hotelData = 'https://test.api.amadeus.com/v1/reference-data/locations/hotels/by-city?cityCode=' + city
     console.log(hotelData)
 
-
-    let response = await fetch(hotelData)
+    let response = await fetch(hotelData,{
+        headers: {Authorization: tokenType} 
+    })
     
     console.log(response.ok)
     if(!response.ok){
@@ -35,6 +38,22 @@ async function cityCoordinates(city){
 
     response = await response.json()
     console.log(response)
+    console.log(response.data.length)
+    if(response.data.length < 50){
+    var iLength = response.data.length
+    }else{
+        iLength = 50
+    }
+
+    for(i=0; i < iLength; i++){
+        var hotelName = response.data[i].name
+        console.log(hotelName)
+        var listI = document.createElement('li')
+        listI.innerHTML = hotelName
+        results.appendChild(listI)
+    }
+
+    
 }
 
 tokenFunction()
